d265b467de3bcaafd1b2083b6e68811d1b0f9e45
KhalilMS3 <kmsfouk@hiof.no>
2024-02-23 06:55:47 +0100

    styling cards

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,14 +4,18 @@ export default function Home({content}){
     return(
         <section>
             <h1>Home</h1>
+            <section className="characters-cards">
+                
             {
                 content?.map(item => 
                 <article key={item.id}>
-                    <h3>{item.name}</h3>
                     <img src={item.image} alt={item.name} />
                     <span className={`status-box ${item.status === "Alive" ? "Alive" : "Dead"}`}></span>
+                    <h3>{item.name}</h3>
+                    <p>{item.status} - {item.species}</p>
                 </article>)
             }
+            </section>
         </section>
     )
 }
